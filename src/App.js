a81184d8f2fee176a9c2319711113f20e82a99db
ogunakar9 <ogunakar09@gmail.com>
2021-08-02 23:38:40 +0300

    add grids and populate them through commerce.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import { commerce } from "./lib/commerce";
+import { Products, Navbar } from "./components"
 function App(){
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+
+    setProducts(data);
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, [])
+
   return(
     <div>
-      <h2>
-        Hello World!
-      </h2>
+      <Navbar />
+      <Products />
     </div>
   );
 }
---
+++ src/components/Products/Products.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Grid } from "@material-ui/core"
+import Product from "./Product/Product";
+import useStyles from "./styles";
+
+const products = [
+  { id: 1, name: "Shoes", description: "Running shoes." },
+  { id: 2, name: "Macbook", description: "Apple macbook." },
+]
+
+const Products = () => {
+  const classes = useStyles();
+
+  return(
+    <main>
+      <div className={classes.toolbar}/>
+      <Grid container justify>
+        {products.map((product) => (
+          <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+            <Product product={product} />
+          </Grid>
+          ))}
+      </Grid>
+    </main>
+  )
+}
+
+export default Products;
