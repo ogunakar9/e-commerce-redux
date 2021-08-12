cf46c74d33e9139317d0256781cd63ba6f5878d7
ogunakar9 <ogunakar09@gmail.com>
2021-08-12 23:41:43 +0300

    add order sagas

--- src/redux/Orders/orderSagas.js
+++ src/redux/Orders/orderSagas.js
@@ -1,25 +1,69 @@
-import ordersTypes from './orderTypes';
+import ordersTypes from './ordersTypes';
+import { takeLatest, put, all, call } from 'redux-saga/effects';
+import { handleSaveOrder, handleGetUserOrderHistory,
+  handleGetOrder } from './orderHelpers';
+import { auth } from '../../firebase/utils';
+import { clearCart } from '../Cart/cartActions';
+import { setUserOrderHistory, setOrderDetails } from './orderActions';
 
-const INITIAL_STATE = {
-  orderHistory: [],
-  orderDetails: {},
+export function* getUserOrderHistory({ payload }) {
+  try {
+    const history = yield handleGetUserOrderHistory(payload);
+    yield put(
+      setUserOrderHistory(history)
+    );
+
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export function* onGetUserOrderHistoryStart() {
+  yield takeLatest(ordersTypes.GET_USER_ORDER_HISTORY_START, getUserOrderHistory);
 };
 
-const ordersReducer = (state=INITIAL_STATE, action) => {
-  switch(action.type) {
-    case ordersTypes.SET_USER_ORDER_HISOTRY:
-      return {
-        ...state,
-        orderHistory: action.payload
-      };
-    case ordersTypes.SET_ORDER_DETAILS:
-      return {
-        ...state,
-        orderDetails: action.payload
-      };
-    default:
-      return state;
+export function* saveOrder({ payload }) {
+  try {
+    const timestamps = new Date();
+    yield handleSaveOrder({
+      ...payload,
+      orderUserID: auth.currentUser.uid,
+      orderCreatedDate: timestamps
+    });
+    yield put(
+      clearCart()
+    )
+
+  } catch (err) {
+    // console.log(err);
   }
 };
 
-export default ordersReducer;
+export function* onSaveOrderHistoryStart() {
+  yield takeLatest(ordersTypes.SAVE_ORDER_HISTORY_START, saveOrder);
+};
+
+export function* getOrderDetails({ payload }) {
+  try {
+    const order = yield handleGetOrder(payload);
+    console.log(order)
+    yield put(
+      setOrderDetails(order)
+    )
+
+  } catch (err) {
+    // console.log(err);
+  }
+}
+
+export function* onGetOrderDetailsStart() {
+  yield takeLatest(ordersTypes.GET_ORDER_DETAILS_START, getOrderDetails);
+};
+
+export default function* ordersSagas() {
+  yield all([
+    call(onSaveOrderHistoryStart),
+    call(onGetUserOrderHistoryStart),
+    call(onGetOrderDetailsStart),
+  ])
+}
